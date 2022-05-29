6a14d4fe77857ad76d6a309e68c5df18d3455ed0
VladimirZR <92719153+VladimirZR@users.noreply.github.com>
2022-05-29 09:42:53 +0200

    Final homework

--- src/main/java/Domaci2705.java
+++ src/main/java/Domaci2705.java
@@ -13,36 +13,52 @@ Kod na git, screen recording puštanja i izvršavanja testa na Drive.
 public class Domaci2705 {
 
     public static void main(String[] args) {
+        try {
+            System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
 
-        System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
 
-        driver.manage().window().maximize();
+            driver.navigate().to("https://www.stealmylogin.com/demo.html");
 
-        driver.navigate().to("https://www.stealmylogin.com/demo.html");
+            //String firstURL = driver.getCurrentUrl();
 
-        String firstURL = driver.getCurrentUrl();
+            WebElement userName = driver.findElement(By.name("username"));
+            userName.sendKeys("Vladimir");
 
-        WebElement userName = driver.findElement(By.name("username"));
-        userName.sendKeys("Vladimir");
+            WebElement password = driver.findElement(By.name("password"));
+            password.sendKeys("Vujin");
 
-        WebElement password = driver.findElement(By.name("password"));
-        password.sendKeys("Vujin");
+            WebElement loginButton = driver.findElement(By.xpath("/html/body/form/input[3]"));
+            loginButton.click();
 
-        WebElement loginButton = driver.findElement(By.xpath("/html/body/form/input[3]"));
-        loginButton.click();
+            String actualURL = driver.getCurrentUrl();
+            String expectingURL = "https://example.com/";
 
-        String newURL = driver.getCurrentUrl();
+            if (actualURL.equals(expectingURL))
+                System.out.println("Nice");
+            else
+                System.out.println("Not nice");
 
-        if(!firstURL.equals(newURL))
-            System.out.println("Nice");
-        else
-            System.out.println("Not nice");
+            /*String newURL = driver.getCurrentUrl();
+            if(!firstURL.equals(newURL))
+                System.out.println("Nice");
+            else
+                System.out.println("Not nice");
 
-        System.out.println("First URL: " + firstURL);
-        System.out.println("New URL: " + newURL);
+            System.out.println("First URL: " + firstURL);
+            System.out.println("New URL: " + newURL);*/
 
-        // Ne traži se u zadatku, uradio sam zbog vežbe
-        // driver.navigate().back();
+            // Ne traži se u zadatku, uradio sam zbog vežbe
+            // driver.navigate().back();
+
+            Thread.sleep(3000);
+
+            driver.close();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
